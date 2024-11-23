e13ac27dd199c030529176d8b525ded8390df1ca
James Tan <jamestansx@gmail.com>
2024-11-24 01:57:01 +0800

    wip: add patient registration endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,46 @@
-use std::{net::SocketAddr, str::FromStr, sync::Arc};
+use std::{net::SocketAddr, str::FromStr};
 
-use axum::{routing::get, Router};
-use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
+use axum::{
+    extract::State,
+    response::Result,
+    routing::post,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use sqlx::{
+    migrate, query,
+    sqlite::SqliteConnectOptions,
+    types::chrono::{DateTime, Utc},
+    FromRow, SqlitePool,
+};
+use tower_http::catch_panic::CatchPanicLayer;
 
-static DB_HOST: &'static str = "sqlite://database.db";
+static DB_HOST: &str = "sqlite://database.db";
 static PORT: u16 = 3000;
 
+#[derive(Clone)]
 struct AppState {
     db: sqlx::Pool<sqlx::Sqlite>,
+}
+
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+struct Patient {
+    id: u16,
+    username: String,
+    first_name: String,
+    last_name: String,
+    #[serde(alias = "birth_of_date")]
+    bod: DateTime<Utc>,
 }
 
 async fn init_db(db_uri: &str) -> Result<SqlitePool, sqlx::Error> {
     let opts = SqliteConnectOptions::from_str(db_uri)?.create_if_missing(true);
 
-    SqlitePool::connect_with(opts).await
+    let pool = SqlitePool::connect_with(opts).await?;
+
+    migrate!("./migrations").run(&pool).await?;
+
+    Ok(pool)
 }
 
 #[tokio::main]
@@ -21,15 +48,19 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
     let pool = init_db(DB_HOST).await?;
-    let app_state = Arc::new(AppState { db: pool });
+    let app_state = AppState { db: pool };
+
+    let api_routes = Router::new().route("/patients/register", post(register_patient));
+    // .route("/queue", get(ws));
 
     let app = Router::new()
-        .route("/", get(|| async { "hello world from qms" }))
-        .with_state(app_state);
+        .nest("/api/v1", api_routes)
+        .with_state(app_state)
+        .layer(CatchPanicLayer::new());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], PORT));
     let listener = tokio::net::TcpListener::bind(addr).await?;
-    tracing::debug!("Listening on {}", listener.local_addr()?);
+    tracing::info!("Listening on {}", listener.local_addr()?);
 
     axum::serve(
         listener,
@@ -38,4 +69,25 @@ async fn main() -> anyhow::Result<()> {
     .await?;
 
     Ok(())
+}
+
+async fn register_patient(
+    State(state): State<AppState>,
+    Json(user): Json<Patient>,
+) -> Result<Json<Patient>> {
+    query!(
+        r#"
+        INSERT INTO patients (first_name, last_name, username, birth_of_date)
+        VALUES (?, ?, ?, ?)
+        "#,
+        user.first_name,
+        user.last_name,
+        user.username,
+        user.bod
+    )
+    .execute(&state.db)
+    .await
+    .expect("Failed to insert new patient value in sqlite");
+
+    Ok(Json(user))
 }
