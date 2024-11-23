f96d3e5615de5e0cc5dbd5df5db440c78ebd2184
James Tan <jamestansx@gmail.com>
2024-11-23 21:25:51 +0800

    feat: hello axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,17 @@
-use std::{net::SocketAddr, path::Path};
+use std::{net::SocketAddr, str::FromStr, sync::Arc};
 
-use axum::{
-    extract::{
-        ws::{Message, WebSocket},
-        ConnectInfo, WebSocketUpgrade,
-    },
-    response::IntoResponse,
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
 
-async fn init_db(filename: impl AsRef<Path>) -> Result<SqlitePool, sqlx::Error> {
-    let opts = SqliteConnectOptions::new()
-        .filename(filename)
-        .create_if_missing(true);
+static DB_HOST: &'static str = "sqlite://database.db";
+static PORT: u16 = 3000;
+
+struct AppState {
+    db: sqlx::Pool<sqlx::Sqlite>,
+}
+
+async fn init_db(db_uri: &str) -> Result<SqlitePool, sqlx::Error> {
+    let opts = SqliteConnectOptions::from_str(db_uri)?.create_if_missing(true);
 
     SqlitePool::connect_with(opts).await
 }
@@ -23,14 +20,15 @@ async fn init_db(filename: impl AsRef<Path>) -> Result<SqlitePool, sqlx::Error> 
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let pool = init_db("database.db").await?;
+    let pool = init_db(DB_HOST).await?;
+    let app_state = Arc::new(AppState { db: pool });
 
     let app = Router::new()
         .route("/", get(|| async { "hello world from qms" }))
-        .route("/ws", get(ws_handler))
-        .with_state(pool);
+        .with_state(app_state);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    let addr = SocketAddr::from(([0, 0, 0, 0], PORT));
+    let listener = tokio::net::TcpListener::bind(addr).await?;
     tracing::debug!("Listening on {}", listener.local_addr()?);
 
     axum::serve(
@@ -40,61 +38,4 @@ async fn main() -> anyhow::Result<()> {
     .await?;
 
     Ok(())
-}
-
-async fn ws_handler(
-    ws: WebSocketUpgrade,
-    ConnectInfo(addr): ConnectInfo<SocketAddr>,
-) -> impl IntoResponse {
-    tracing::debug!("{addr} has connected!");
-    ws.on_upgrade(move |socket| handle_socket(socket, addr))
-}
-
-async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
-    if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
-        tracing::info!("Pinged {who}...");
-    } else {
-        tracing::warn!("Could not pinged {who}!");
-        return;
-    }
-
-    while let Some(msg) = socket.recv().await {
-        if let Ok(msg) = msg {
-            match msg {
-                Message::Ping(t) => {
-                    tracing::info!("{who} pinged us with {t:?}!");
-                }
-                Message::Pong(v) => {
-                    if socket
-                        .send(Message::Text(format!(
-                            "Hello from websocket, this is your pong: {v:?}"
-                        )))
-                        .await
-                        .is_err()
-                    {
-                        tracing::error!("Failed to send message to {who}");
-                    }
-                }
-                Message::Text(v) => {
-                    tracing::info!("Recieved message from {who}");
-                    if socket
-                        .send(Message::Text(format!(
-                            "Hello from websocket, this is your pong: {v:?}"
-                        )))
-                        .await
-                        .is_err()
-                    {
-                        tracing::error!("Failed to send message to {who}");
-                    }
-                }
-                Message::Close(v) => {
-                    tracing::info!("BYE {v:?}!");
-                    return;
-                }
-                m => {
-                    tracing::warn!("{m:?} is not implemented!");
-                }
-            }
-        }
-    }
 }
