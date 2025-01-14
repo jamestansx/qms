0986acbf5a2ebcc143c00822cab0dfca4ace92c8
James Tan <jamestansx@gmail.com>
2025-01-14 22:53:57 +0800

    update

---
+++ backend/migrations/20250114141943_insert_dummy_patients.sql
@@ -0,0 +1,7 @@
+INSERT INTO patients (username, first_name, last_name, password, date_of_birth) VALUES
+("james", "James", "Tan", "1234", "2002-01-24"),
+("weihan", "Ng", "Wei Han", "1234", "2002-02-13"),
+("weile", "Chang", "Wei Le", "1234", "2002-09-06"),
+("elderly", "Old", "Man", "1234", "1955-04-30"),
+("iu", "Lee", "Ji Eun", "uaena", "1993-05-16"),
+("younice", "Eunice", "Chua", "0430", "2002-04-30");
